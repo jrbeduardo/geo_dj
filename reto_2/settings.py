537bd7ff81a58b2d9acabe4c2c8e57805c1a7d9f
Eduardo <jrbeduardo@gmail.com>
2021-08-24 21:10:49 -0500

    forth commit

--- reto_2/settings.py
+++ reto_2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,5 @@ GEOIP_PATH= os.path.join(BASE_DIR, 'geoapi')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+django_heroku.settings(locals())
