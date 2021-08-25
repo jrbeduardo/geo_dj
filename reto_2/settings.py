aeb63c1be2c3d5dfadcf9ddb5ddff3fc0dec1dfe
Eduardo <jrbeduardo@gmail.com>
2021-08-24 21:22:38 -0500

    six commit

--- reto_2/settings.py
+++ reto_2/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sy=6%+r2psw$)ps#c%kk$(h0emz1#f93*it=(da^sdrf0zyvq0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
