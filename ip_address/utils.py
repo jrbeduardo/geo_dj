82a0fa97ca076ceb29022e60ec68280787b232be
Eduardo <jrbeduardo@gmail.com>
2021-08-24 21:59:53 -0500

    8 commit

--- ip_address/utils.py
+++ ip_address/utils.py
@@ -29,7 +29,7 @@ def get_zoom(distance):
 def get_ip_address(request):
     x_forwarded_for = request.META.get('HTTP_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for.split(',')[-1].strip()
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
